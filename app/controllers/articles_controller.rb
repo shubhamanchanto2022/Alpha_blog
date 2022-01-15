48557a1849525ee8747666395a9e73f05e301930
shubhamanchanto2022 <shubham.sinha@anchanto.com>
2022-01-15 13:37:10 +0530

    updating in database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,14 @@ class ArticlesController<ApplicationController
     def index
         @articles=Article.all
     end
+
+    def new
+
+    end
+
+    def create
+        @article =Article.new(params.require(:article).permit(:Title, :Description))
+        @article.save
+        redirect_to article_path(@article) # redirecting to show action
+    end
 end
