66198b0d0d8c4f1adc92f5063dfa7ccc65e5f18a
shubhamanchanto2022 <shubham.sinha@anchanto.com>
2022-01-15 22:56:58 +0530

    added crud operations of delete and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,32 @@ class ArticlesController<ApplicationController
         @articles=Article.all
     end
 
-    def new
+    def edit
+        @article=Article.find(params[:id])
+    end
 
+    def new
+        @article=Article.new
     end
 
     def create
         @article =Article.new(params.require(:article).permit(:Title, :Description))
-        @article.save
-        redirect_to article_path(@article) # redirecting to show action
+        if @article.save
+            flash[:notice]= "Article was created successfully."
+            redirect_to article_path(@article) # redirecting to show action
+        else 
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:Title, :Description))
+            flash[:notice]= "Article was updated successfully"
+            redirect_to @article
+        else 
+            render 'edit'
+        end
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
    root 'pages#home' # before '#' that is the name of controller, so for our application it is pages which we can see in controllers
    get 'about', to: 'pages#about'
-   resources :articles, only: [:show , :index , :new , :create]
+   resources :articles, only: [:show , :index , :new , :create, :edit, :update]
 end
